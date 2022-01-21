04369c4d2f8eaa2aee8dc8d16ff8e0dfc5baf45d
Younggregs <dretzam@gmail.com>
2022-01-21 11:37:09 +0100

    update db

--- connection/db_config.php
+++ connection/db_config.php
@@ -4,8 +4,8 @@
  * All database connection variables
  */
  
-define('DB_USER', "sql11457931"); // db user
-define('DB_PASSWORD', "VfvifjN7cL"); // db password (mention your db password here)
-define('DB_DATABASE', "sql11457931"); // database name
-define('DB_SERVER', "sql11.freesqldatabase.com"); // db server
+define('DB_USER', "sql11467061"); // db user
+define('DB_PASSWORD', "LVR5tke8n5"); // db password (mention your db password here)
+define('DB_DATABASE', "sql11467061"); // database name
+define('DB_SERVER', "sql11.freemysqlhosting.net"); // db server
 ?>
